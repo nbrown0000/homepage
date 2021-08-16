624f36192701e9327e0a3ab0355a0a4130723b10
Nathan Brown <brown.nathanjames@gmail.com>
2021-08-16 15:45:28 +1000

    Add routes with express-session

--- app.js
+++ app.js
@@ -1,10 +1,97 @@
 const express = require('express')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+
+const DAY =  1000 * 60 * 60 * 24
 
 const {
-  PORT = 3000
+  PORT = 3000,
+  NODE_ENV = 'development',
+  SESS_NAME = 'sid',
+  SESS_SECRET = 'something!that$is%secret&',
+  SESS_LIFETIME = DAY,
+  DB_URL = 'mongodb://localhost/test-app',
+  DB_PORT = '27017',
+  DB_NAME = 'homepage'
 } = process.env
 
+const IN_PROD = NODE_ENV === 'production'
+
+// TODO: DB
+const users = [
+  { id: 1, name: 'Alex', email: 'alex@gmail.com', password: 'secret' },
+  { id: 2, name: 'Max', email: 'max@gmail.com', password: 'secret' },
+  { id: 3, name: 'Bruce', email: 'bruce@gmail.com', password: 'secret' }
+]
+
 const app = express()
+
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.use(session({
+  name: SESS_NAME,
+  resave: false,
+  saveUninitialized: false,
+  secret: SESS_SECRET,
+  store: MongoStore.create({ mongoUrl: DB_URL }),
+  cookie: {
+    maxAge: SESS_LIFETIME,
+    sameSite: true, // TODO: CSRF
+    secure: IN_PROD
+  }
+}))
+
+app.get('/', (req, res) => {
+  const { userId } = req.session
+  if (userId) {
+    res.redirect('/dashboard')
+  }
+  res.redirect('/login')
+})
+
+app.get('/dashboard', (req, res) => {
+  res.send(`
+    <h1>Dashboard</h1>
+    <p>Weather</p>
+    <p>News</p>
+    <p>Todo List</p>
+  `)
+})
+
+app.get('/login', (req, res) => {
+  res.send(`
+    <h1>Login</h1>
+    <form method='post' action='/login'>
+      <input type='email' name='email' placeholder='Email' required />
+      <input type='password' name='password' placeholder='Password' required />
+      <input type='submit' />
+    </form>
+    <a href='/register'>Register</a>
+  `)
+})
+
+app.get('/register', (req, res) => {
+  res.send(`
+    <h1>Register</h1>
+    <form method='post' action='/register'>
+      <input name='name' placeholder='Name' required />
+      <input type='email' name='email' placeholder='Email' required />
+      <input type='password' name='password' placeholder='Password' required />
+      <input type='submit' />
+    </form>
+    <a href='/login'>Login</a>
+  `)
+})
+
+app.post('/login', (req, res) => {
+
+})
+
+app.post('/register', (req, res) => {
+
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
