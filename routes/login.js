bef20f60451d31c63802bfc90c132d119d161535
Nathan Brown <brown.nathanjames@gmail.com>
2021-08-26 14:29:50 +1000

    Add input validation to login route
    Login route require inputs validated. Added validation.

--- routes/login.js
+++ routes/login.js
@@ -2,11 +2,20 @@ const express = require('express')
 const router = express.Router()
 const MongoClient = require('mongodb').MongoClient
 const bcrypt = require('bcrypt');
+const { check, validationResult } = require('express-validator')
 const { redirectHome } = require('../middleware/redirects')
 const {
   DB_URL,
   DB_NAME,
 } = require('../config')
+
+const loginValidate = [
+  check('email')
+    .isEmail().withMessage('Must be valid email')
+    .trim().escape().normalizeEmail(),
+  check('password')
+    .trim().escape()
+]
 
 router.get('/', redirectHome, (req, res) => {
   const error = req.session.error || ''
@@ -14,33 +23,37 @@ router.get('/', redirectHome, (req, res) => {
   res.render('login', { error })
 })
 
-router.post('/', (req, res) => {
+router.post('/', loginValidate, (req, res) => {
+  const validationErrors = validationResult(req)
+  if (!validationErrors.isEmpty()) {
+    const errorMessage = validationErrors.array().reduce((acc, cur) => {
+      return acc + cur.msg + '. '
+    }, '')
+    req.session.error = `${errorMessage}`
+    return res.redirect('/login')
+  }
+
   const { email, password } = req.body
-  if (email && password) { // TODO: validation
 
-    MongoClient.connect(DB_URL, (err, client) => {
-      if (err) return console.log(err)
-      db = client.db(DB_NAME)
-      db.collection('users')
-        .findOne({ "email": email })
-        .then(async user => {
-          
-          const match = await bcrypt.compare(password, user.password)
-          if (user.email === email && match) {
-            req.session.error = ''
-            req.session.userId = user._id.toHexString()
-            return res.redirect('/')
-          }
-          req.session.error = 'Incorrect username or password'
-          res.redirect('/login')
-        })
-        .catch(err => console.error(err))
-    })
+  MongoClient.connect(DB_URL, (err, client) => {
+    if (err) return console.log(err)
+    db = client.db(DB_NAME)
+    db.collection('users')
+      .findOne({ "email": email })
+      .then(async user => {
+        
+        const match = await bcrypt.compare(password, user.password)
+        if (user.email === email && match) {
+          req.session.error = ''
+          req.session.userId = user._id.toHexString()
+          return res.redirect('/')
+        }
+        req.session.error = 'Incorrect username or password'
+        res.redirect('/login')
+      })
+      .catch(err => console.error(err))
+  })
 
-  }
-  else {
-    res.status(401).json({ error: 'Username or password not supplied'})
-  }
 })
 
 module.exports = router
