11771e4b9068d0fb3f40b346b9c7098fc319bb51
Nathan Brown <brown.nathanjames@gmail.com>
2021-09-17 22:38:25 +1000

    Renamed todos section to tasks section
    Todo list will comprise of two parts: Tasks and Todos. Each Task will contain up to multiple Todos. In keeping with this organisation of todos the todos section has been renamed tasks section.

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,17 +1,17 @@
 function showWeather() {
   document.getElementsByClassName('weather-section')[0].classList.remove('hidden-mobile');
   document.getElementsByClassName('news-section')[0].classList.add('hidden-mobile');
-  document.getElementsByClassName('todos-section')[0].classList.add('hidden-mobile');
+  document.getElementsByClassName('tasks-section')[0].classList.add('hidden-mobile');
 }
 
 function showNews() {
   document.getElementsByClassName('weather-section')[0].classList.add('hidden-mobile');
   document.getElementsByClassName('news-section')[0].classList.remove('hidden-mobile');
-  document.getElementsByClassName('todos-section')[0].classList.add('hidden-mobile');
+  document.getElementsByClassName('tasks-section')[0].classList.add('hidden-mobile');
 }
 
-function showTodos() {
+function showTasks() {
   document.getElementsByClassName('weather-section')[0].classList.add('hidden-mobile');
   document.getElementsByClassName('news-section')[0].classList.add('hidden-mobile');
-  document.getElementsByClassName('todos-section')[0].classList.remove('hidden-mobile');
+  document.getElementsByClassName('tasks-section')[0].classList.remove('hidden-mobile');
 }
