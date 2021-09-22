c9970b84ceceac65e5b56b56344c6feb6de68bbc
Nathan Brown <brown.nathanjames@gmail.com>
2021-09-22 22:31:56 +1000

    Change news fetching to be based on country
    News fetching was hardcoded to location Australia (AU). Changed to lookup users country in DB and use this to get locational country headlines.

--- middleware/news.js
+++ middleware/news.js
@@ -19,8 +19,25 @@ const getNewsData = async (req, res, next) => {
     return
   }
 
+  // get country data from user account
+  let userCountry
+  try {
+    const db = client.db(DB_NAME)
+    let collection = db.collection('users')
+    let query = { _id: ObjectId(req.session.userId) }
+    const response = await collection.findOne(query)
+    const { country } = response
+    userCountry = country
+
+  } catch (err) { return console.error(err) }
+
+  // convert country name to country code
+  const countryData = require('../data/countryData')
+  const data = await countryData.find(c => c.Name.toLowerCase() === userCountry.toLowerCase())
+  const countryCode = data.Code
+
   // Get weather data
-  const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=au&apiKey=${NEWS_API_KEY}`)
+  const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${countryCode}&apiKey=${NEWS_API_KEY}`)
 
   // Store weather in res.locals so '/' route can read and pass it to views
   res.locals.newsData = response.data
